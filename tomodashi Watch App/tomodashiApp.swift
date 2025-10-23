2ddd604e11739922042df1fb3e66ec295d2d9b60
cranberrymuffin <aparnalovestocode@gmail.com>
2025-10-23 10:08:40 -0400

    Initial Commit

---
+++ tomodashi Watch App/tomodashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tomodashiApp.swift
+//  tomodashi Watch App
+//
+//  Created by Aparna Natarajan on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct tomodashi_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
